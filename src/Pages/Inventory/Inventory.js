d24b4d73dac9ccbf1c6e554ba6329e5d711e7667
Almas Khan <almas.rbm@gmail.com>
2022-05-09 01:05:52 +0600

    Restock item updated

--- src/Pages/Inventory/Inventory.js
+++ src/Pages/Inventory/Inventory.js
@@ -1,18 +1,73 @@
 import React, { useEffect, useState } from 'react';
 import { useParams } from 'react-router-dom';
+import { useForm } from "react-hook-form";
 import './Inventory.css';
 const Inventory = () => {
     const { inventoryId } = useParams();
+    const { register, handleSubmit } = useForm();
     const [item, setItem] = useState([]);
+    const [quantity, setQuantity] = useState({});
     useEffect(() => {
         const url = `http://localhost:5000/item/${inventoryId}`;
         fetch(url)
             .then(res => res.json())
             .then(data => setItem(data))
     }, []);
+
+    useEffect(() => {
+        const url = `http://localhost:5000/item/${inventoryId}`;
+        fetch(url)
+            .then(res => res.json())
+            .then(data => setQuantity(data))
+    }, []);
+
+    const handleDelivered = id => {
+        // console.log(id);
+        const previousQuantity = parseInt(item.quantity);
+        const newQuantity = previousQuantity - 1;
+        // setQuantity(newQuantity);
+        // console.log(newQuantity);
+        const quantities = { newQuantity };
+        console.log(quantities);
+
+        const url = `http://localhost:5000/item/${inventoryId}`;
+        fetch(url, {
+            method: 'PUT',
+            headers: {
+                'Content-Type': 'application/json',
+            },
+            body: JSON.stringify(quantities),
+        })
+            .then(res => res.json())
+            .then(data => {
+                console.log(data);
+                alert('Item updated successfully!!!');
+            })
+    }
+    //Update quantity
+    const onSubmit = data => {
+        console.log(data);
+        //send data to the server...
+        //search mdn fetch post
+        //sending data
+        const url = `http://localhost:5000/item/${inventoryId}`;
+        fetch(url, {
+            //PUT for updating...
+            method: 'PUT',
+            headers: {
+                'Content-Type': 'application/json',
+            },
+            body: JSON.stringify(data),
+        })
+            .then(res => res.json())
+            .then(data => {
+                alert('Item updated successfully!!!');
+                data.reset();
+            })
+    };
     return (
-        <div className='w-50 mx-auto'>
-            <h1 className='text-center mt-3 mb-5'>Details of the Inventory Item:</h1>
+        <div className='w-50 mx-auto mb-3'>
+            <h1 className='text-center mt-3 mb-4'>Details of the Inventory Item:</h1>
             <div className='item-details'>
                 <div className='w-50 mx-auto'>
                     <img className='mb-4' src={item.img} alt="" />
@@ -22,7 +77,17 @@ const Inventory = () => {
                 <h4 className='text-center'>Price: {item.price}</h4>
                 <h4 className='text-center'>Quantity: {item.quantity}</h4>
                 <h3 className='text-center'>Supplier Name: {item.supplierName}</h3>
-                <p className='fs-5 text-success mb-5 mt-4'>Description: {item.description}</p>
+                <p className='fs-5 text-success mb-2 mt-2'>Description: {item.description}</p>
+                <div className='d-flex justify-content-center'>
+                    <button className='rounded' onClick={() => handleDelivered(item._id)}>Delivered</button>
+                </div>
+                <div className='w-50 mx-auto'>
+                    <h3 className='mt-4'>Restock Items</h3>
+                    <form className='d-flex flex-column mb-5' onSubmit={handleSubmit(onSubmit)}>
+                        <input className='mb-2' placeholder='Quantity' type="number" {...register("quantity", { required: true })} />
+                        <input className='w-50 mt-3 bg-primary rounded border-0 p-2 text-white mx-auto' type="submit" value="Update Quantity" />
+                    </form>
+                </div>
             </div>
         </div>
     );
